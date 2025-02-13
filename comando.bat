43bf52e939fe2757b2d7bb37d9ef4efad6b19674
Lara <lara.a.silva12@aluno.senai.br>
2025-02-13 15:02:23 -0300

    Add files via upload

--- comando.bat
+++ comando.bat
@@ -4,3 +4,16 @@ mkdir terror
 mkdir romance
 cd romance
 echo a escolha > meusfilmes.txt
+echo a culpa das estrelas >> meusfilmes.txt
+echo a 5 passos de você >> meusfilmes.txt
+cd ..
+cd terror 
+echo anabelle > filmesdeterror.txt
+echo a casa de cera >> filmesdeterror.txt
+echo bird box >> filmesdeterror.txt
+cd ..
+mkdir infantil
+cd infantil
+echo a princesa e o sapo > filmesinfantis.txt
+echo shreak >> filmesinfantis.txt
+echo enrolados >> filmesinfantis.txt
